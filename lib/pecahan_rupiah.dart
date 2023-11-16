c3f91001abe2ce5f11ec1cf827bd9a2fc6b019a9
zhuyuhui <zhuyuhui@odianyun.com>
2023-11-16 14:39:39 +0800

    update to support null_safety

--- lib/pecahan_rupiah.dart
+++ lib/pecahan_rupiah.dart
@@ -1,12 +1,11 @@
 library pecahan_rupiah;
 
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class Pecahan{
- 
-  static String rupiah({@required int value, bool withRp}){
-    final String hasil = NumberFormat("#,###").format(value).replaceAll(",", ".");
-    return withRp == null || !withRp ?hasil: "Rp $hasil";
+class Pecahan {
+  static String rupiah({required int value, bool? withRp}) {
+    final String hasil =
+        NumberFormat("#,###").format(value).replaceAll(",", ".");
+    return withRp == null || !withRp ? hasil : "Rp $hasil";
   }
 }
